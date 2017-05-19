051b8ef84aa019b8d6e5986992258df17bf00917
turboTy <845383047@qq.com>
2017-05-20 00:51:16 +0800

    add bootstrap
    2017年5月20日 00:51:14

--- configs/db_conn.php
+++ configs/db_conn.php
@@ -1,15 +1,15 @@
 <?php
-error_reporting('E_ALL^ E_NOTICE');
-// include "../configs/configs.php";
+// error_reporting('E_ALL^ E_NOTICE');
+require_once ("configs.php");
 
 //判断 "DB_HOST"是否定义成功
 /* if(defined("DB_HOST")){
     echo "***";
 } */
 
-$db = new mysqli("localhost","root","sql845383047","blog");
-// $db = new mysqli(DB_HOST,DB_USER,DB_PWD,DB_DBNAME)
-if (mysqli_connect_errno()){
+//连接数据库
+$db = new mysqli(DB_HOST,DB_USER,DB_PWD,DB_DBNAME);
+if(mysqli_connect_errno()){
     die('连接数据库失败Error:'.mysqli_connect_errno().":".mysqli_connect_error());
 }
 mysqli_set_charset($db,DB_CHARSET);
