83a00f31cf97b2cbcfa22536a5d1680977a743fa
turboTy <845383047@qq.com>
2017-05-19 23:30:25 +0800

    Add files via upload

---
+++ include/Smarty_inc.php
@@ -0,0 +1,25 @@
+<?php
+//引入Smarty模板类
+require_once("../smarty/libs/Smarty.class.php");
+//实例化Smarty类对象
+// $smarty=new Smarty;
+//是否使用缓存，项目调试期间，不建议启用缓存
+// $smarty->caching=false;
+//缓存生命周期
+// $smarty->cache_lifetime=120;
+//设置配置目录
+// $smarty->setConfigDir("../configs");
+//设置模板目录
+//$smarty->template_dir="../templates";
+                                    // $smarty->setTemplateDir("../templates");
+//设置编译目录351721199305168049
+//$smarty->compile_dir="../templates_c";
+                                    // $smarty->setCompileDir("../templates_c");
+//缓存文件夹
+//$smarty->cache_dir="../smarty_cache";
+                                    // $smarty->setCacheDir("../smarty_cache");
+                                    
+//左右边界符,默认是{},但实际应用中容易与JavaScript冲突，可以设计为<{ and }>这样的形式
+//$smarty->left_delimiter="{";
+//$smarty->right_delimiter="}";
+?>
