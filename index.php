2bddf13ee0b5076b3a0b5b030ac27c0353cb2e19
turboTy <845383047@qq.com>
2017-09-26 22:37:49 +0800

    update t_index.html
    add main-content-t

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ require_once("./configs/db_conn.php");
 require_once("libs/mysql.func.php");
 require_once("./inc/global_params.php");
 
-//sql_select_getonevalue("blog_users", "user_name", "id = '2'", $name);
-//sql_select_get2value("blog_users", "user_name", "user_password", "id = '2'", $name, $password);
 sql_leftjoin_getonevalue("blog_users a", "user_info b", "a.user_id = b.userid", "b.id = '1'", $array);
 
 add_option("id", "user_name", "blog_users", "1 order by id asc", "2", $value);
