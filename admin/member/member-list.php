6cc5afebae399bf0c7ee010447e830da7802dd84
turboTy <845383047@qq.com>
2018-01-24 10:33:02 +0800

    Update member-list.php

--- admin/member/member-list.php
+++ admin/member/member-list.php
@@ -1,1 +1,23 @@
 <?php
+require_once("../../smarty/libs/Smarty.class.php");
+require_once ("../../configs/db_conn.php");
+require_once ("../../inc/global_params.php");
+require_once '../../libs/mysql.func.php';
+
+class MemberList
+{
+  protected $db;
+  
+  public function __construct($db)
+  {
+    this->$db = $db;
+  }
+  
+  public function member_list()
+  {
+     
+  }
+  
+}
+
+
